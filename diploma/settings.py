334fc98836059628c995cde8f3527921665bbd51
Dmitry Gavrilenko <d.gavrilenko@invento.by>
2017-02-12 15:40:50 +0300

    init

--- diploma/settings.py
+++ diploma/settings.py
@@ -72,9 +72,6 @@ WSGI_APPLICATION = 'diploma.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-
-
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
