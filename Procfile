86a7d1cfdee7d162b4a41dcd911b5d7d18b93f80
Dmitry Gavrilenko <d.gavrilenko@invento.by>
2017-02-12 15:07:38 +0300

    asd

--- Procfile
+++ Procfile
@@ -1,3 +1,2 @@
-web: pip install -r requirements.txt
 web: python manage.py migrate
-web: python manage.py runserver 0.0.0.0:5000
+web: python manage.py runserver "0.0.0.0:$PORT"
--- diploma/settings.py
+++ diploma/settings.py
@@ -74,15 +74,17 @@ WSGI_APPLICATION = 'diploma.wsgi.application'
 
 import dj_database_url
 
-DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
+# DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
+#
 
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 DATABASES['default'] = dj_database_url.config()
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 # 'default': {
 #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
 #     'NAME': os.getenv('POSTGRES_DB', 'takenote'),
