fa631b24755f1659335c7a4d10eadf80abad3273
Dmitry Gavrilenko <d.gavrilenko@invento.by>
2017-02-14 20:01:32 +0300

    ad

--- management/admins/car.py
+++ management/admins/car.py
@@ -20,15 +20,15 @@ class CarAdmin(admin.ModelAdmin):
         'year',
         'status',
     )
-    # search_fields = ('rents__start_date',)
-    #
-    # def get_search_results(self, request, queryset, search_term):
-    #     queryset, use_distinct = super(CarAdmin, self).get_search_results(request, queryset, search_term)
-    #     if not use_distinct:
-    #         return queryset, use_distinct
-    #     else:
-    #         qs = Car.objects.all().exclude(rents__start_date__lte=use_distinct, rents__end_date__gte=use_distinct)
-    #         return qs,use_distinct
+    search_fields = ('rents__start_date',)
+
+    def get_search_results(self, request, queryset, search_term):
+        queryset, use_distinct = super(CarAdmin, self).get_search_results(request, queryset, search_term)
+        if not use_distinct:
+            return queryset, use_distinct
+        else:
+            qs = Car.objects.all().exclude(rents__start_date__lte=search_term, rents__end_date__gte=search_term)
+            return qs,use_distinct
 
     fields = (
         'make',
