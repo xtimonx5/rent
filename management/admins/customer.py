5d6a2aa1a151415e351e9f65f7ead08bed043571
Dmitry Gavrilenko <d.gavrilenko@invento.by>
2017-03-26 14:11:16 +0300

    ad

--- management/admins/customer.py
+++ management/admins/customer.py
@@ -7,9 +7,15 @@ class RentInline(admin.TabularInline):
     model = Rent
     extra = 0
     can_delete = False
+    can_add = False
+
 
     def has_add_permission(self, request):
         return False
+
+    def has_module_permission(self, request):
+        return True
+
 
     readonly_fields = (
         'car',
@@ -17,6 +23,9 @@ class RentInline(admin.TabularInline):
         'status',
         'comment',
     )
+
+    verbose_name = 'History of activity'
+    verbose_name_plural = 'History of activity'
 
 
 class CustomerAdmin(admin.ModelAdmin):
