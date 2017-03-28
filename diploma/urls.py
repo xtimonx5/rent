dd6066dd0622b9322424031e97d1c398379f9067
Dmitry Gavrilenko <d.gavrilenko@invento.by>
2017-03-28 12:20:27 +0300

    a

--- diploma/urls.py
+++ diploma/urls.py
@@ -32,4 +32,4 @@ urlpatterns = [
     url(r'^about/$', about, name='about'),
     url(r'^cars/$', cars, name='cars')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-              # + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
+    # static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
