f181378276c8553b2c60c780ada2f7f03342dc6e
Dmitry Gavrilenko <d.gavrilenko@invento.by>
2017-05-26 14:09:02 +0300

    foo

--- management/models/customer.py
+++ management/models/customer.py
@@ -6,6 +6,7 @@ class Customer(models.Model):
     last_name = models.CharField(max_length=50, null=True, blank=True)
     phone = models.CharField(max_length=50)
     passport = models.CharField(max_length=50, null=True, blank=True)
+    address = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.first_name + ' ' + self.last_name
